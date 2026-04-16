41131e7c79e16ccda7a77c152fc32412a8e2a36c
snolls <alexperezsoto@gmail.com>
2026-04-16 21:41:09 +0200

    Reserva de recursos

--- src/main/java/com/aulaclick/service/ReservaService.java
+++ src/main/java/com/aulaclick/service/ReservaService.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
